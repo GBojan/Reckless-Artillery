ae742e595cb136d96484de1213f9f00e57493821
Bojan Gigovski <gigovski_bojan@outlook.com>
2018-05-21 13:54:10 +0200

    Fixed bug where archers fired at the same time

---
+++ Assets/Scripts/Characters/AttackingCharacters.cs
@@ -0,0 +1,69 @@
+﻿using Assets.Classes.Artillery;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingCharacters : MonoBehaviour {
+    public int projectileSpeed;
+    public int projectileHeightFrom;
+    public int projectileHeightTo;
+    public GameObject projectile;
+    private ShootArrows shootArrows;
+	// Use this for initialization
+	void Start () {
+
+        shootArrows = new ShootArrows()
+        {
+            arrowToShoot = projectile,
+            HeightFrom = projectileHeightFrom,
+            HeightTo = projectileHeightTo,
+            Speed = 150,
+            projectileFired = false
+        };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (shootArrows.projectileFired == false)
+        {
+            //Time between shooting arrows
+            var seconds = Random.Range(2.0f, 5.0f);
+            StartCoroutine(ShootArrow(seconds));
+        }
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * projectileSpeed * Time.deltaTime);
+        }
+    }
+    void Shooting()
+    {
+        if (shootArrows.projectileFired == false)
+        {
+            Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+    IEnumerator ShootArrow(float seconds)
+    {
+        shootArrows.projectileFired = true;
+    
+        Debug.Log("Seconds to trigger: " + seconds);
+        yield return new WaitForSeconds(seconds);
+        print(gameObject.name);
+
+        //Spawning an arrow
+        Vector3 newVector = new Vector3(this.transform.position.x, this.transform.position.y);
+        GameObject newArrow = Instantiate(shootArrows.arrowToShoot, newVector, this.transform.rotation);
+
+        var height = Random.Range(shootArrows.HeightFrom, shootArrows.HeightTo);
+        var gmobject = new Arrow(2, shootArrows.Speed, height, 2, newArrow);
+        gmobject.ShootMe();
+        shootArrows.projectileFired = false;
+    }
+}
--- Assets/Scripts/Characters/ShootArrows.cs
+++ Assets/Scripts/Characters/ShootArrows.cs
@@ -8,26 +8,28 @@ public class ShootArrows : MonoBehaviour {
     public int HeightFrom, HeightTo;
     public int Speed;
     public GameObject arrowToShoot;
-    bool projectileFired = false;
+    public bool projectileFired = false;
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(this.ShootArrow());
+        //StartCoroutine(this.ShootArrow());
     }
 	
 	// Update is called once per frame
 	void Update () {
-     
-	}
-  
+        if (projectileFired==false)
+        {
+            StartCoroutine("ShootArrow");
+        }
+    }
     IEnumerator ShootArrow()
     {
-        while (true)
-        {
+            projectileFired = true;
             //Time between shooting arrows
             System.Random random = new System.Random();
-            var seconds = random.Next(1, 4);
-            Debug.Log("Seconds to trigger: "+seconds);
-            yield return new WaitForSeconds(3);
+            var seconds = random.Next(3, 5);
+            Debug.Log("Seconds to trigger: " + seconds);
+            yield return new WaitForSeconds(seconds);
+            print(gameObject.name);
 
             //Spawning an arrow
             Vector3 newVector = new Vector3(this.transform.position.x, this.transform.position.y);
@@ -35,10 +37,9 @@ public class ShootArrows : MonoBehaviour {
 
             System.Random randomHeight = new System.Random();
             var height = random.Next(HeightFrom, HeightTo);
-            Debug.Log("My arrow height: "+height);
             var gmobject = new Arrow(2, Speed, height, 2, newArrow);
             gmobject.ShootMe();
-        }
+            projectileFired = false;
     }
 
 }
