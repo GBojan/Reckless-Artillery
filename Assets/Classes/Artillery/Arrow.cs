e40d53eec4eddc8c43d55f1ad869dcc836a377c2
Bojan Gigovski <gigovski_bojan@outlook.com>
2018-05-21 12:11:12 +0200

    First commit

---
+++ Assets/Classes/Artillery/Arrow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Classes.Artillery
+{
+    public class Arrow
+    {
+        //Health shows us how many times we need to tap the object to destroy it 
+        public int Health { get; set; }
+
+        public float Speed { get; set; }
+
+        public int Height { get; set; }
+
+        public float Damage { get; set; }
+
+        public GameObject GameObj { get; set; }
+
+        public float lifeSpan { get; set; }
+
+        public Arrow(int health,float speed, int height, float damage, GameObject gameObject)
+        {
+            Health = health;
+            Speed = speed;
+            Height = height;
+            Damage = damage;
+            GameObj = gameObject;
+            lifeSpan = 3f;
+        }
+
+        public void ShootMe()
+        {
+            //Adding rotation
+            var objectRigidBody = GameObj.GetComponent<Rigidbody2D>();
+            Vector2 vector = objectRigidBody.velocity;
+            float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+            objectRigidBody.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            //Adding speed
+            objectRigidBody.AddForce(Vector2.right * Speed);
+            objectRigidBody.AddForce(Vector2.up * Height);
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/ShootArrows.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Classes;
+using Assets.Classes.Artillery;
+
+public class ShootArrows : MonoBehaviour {
+    public int HeightFrom, HeightTo;
+    public int Speed;
+    public GameObject arrowToShoot;
+    bool projectileFired = false;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(this.ShootArrow());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     
+	}
+  
+    IEnumerator ShootArrow()
+    {
+        while (true)
+        {
+            //Time between shooting arrows
+            System.Random random = new System.Random();
+            var seconds = random.Next(1, 4);
+            Debug.Log("Seconds to trigger: "+seconds);
+            yield return new WaitForSeconds(3);
+
+            //Spawning an arrow
+            Vector3 newVector = new Vector3(this.transform.position.x, this.transform.position.y);
+            GameObject newArrow = Instantiate(arrowToShoot, newVector, this.transform.rotation);
+
+            System.Random randomHeight = new System.Random();
+            var height = random.Next(HeightFrom, HeightTo);
+            Debug.Log("My arrow height: "+height);
+            var gmobject = new Arrow(2, Speed, height, 2, newArrow);
+            gmobject.ShootMe();
+        }
+    }
+
+}
